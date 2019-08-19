e07b07f0a4d17de7ecba250e9e46fd7fc2067dbe
howeycheng <howeycheng@163.com>
2019-08-19 18:35:29 +0800

    add apitest

---
+++ frontend/src/test/index.js
@@ -0,0 +1,18 @@
+// var app = new Vue(
+//   {
+//     el: '#app',
+//     data: {
+//       message: 'Hello'
+//     }
+//   }
+// )
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date().toLocaleString()
+  }
+
+})
+
+// app.message = 'I'
