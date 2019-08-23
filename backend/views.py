9b0538d6d3679b7130a17377de4cdae4d6f3c701
howeycheng <howeycheng@163.com>
2019-08-23 17:57:05 +0800

    django和vue数据交互测试

--- backend/views.py
+++ backend/views.py
@@ -1,18 +1,9 @@
-import json
-
-from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Requirement
-from .models import Scene
-
-from django.views.decorators.csrf import csrf_exempt
 
 
 @api_view(['GET', 'POST'])
@@ -22,4 +13,11 @@ def get_req(request):
         cases = Requirement.objects.filter(parent_id=0).values_list('rqid', 'name', 'parent_id')
     elif rqid is not None:  # 查询点击节点子需求
         cases = Requirement.objects.filter(parent_id=rqid).values_list('rqid', 'name', 'parent_id')
+    return Response(cases)
+
+
+@api_view(['GET', 'POST'])
+def get_scene(request):
+    rqid = request.GET.get('rqid')  # 需求id
+    cases = Requirement.objects.filter(parent_id=rqid).values_list('rqid', 'name', 'parent_id')
     return Response(cases)
