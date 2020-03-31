80c23075681404fbd41aa9d537b7f48772afce67
howey <howeycheng@163.com>
2020-03-31 20:29:02 +0800

    完成测试集下用例查询接口

--- backend/rocketmq/producer.py
+++ backend/rocketmq/producer.py
@@ -3,8 +3,7 @@ from rocketmq.client import Producer, Message
 producer = Producer('PID-XXX')
 producer.set_namesrv_domain('http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet')
 # For ip and port name server address, use `set_namesrv_addr` method, for example:
-producer.set_namesrv_addr('10.1.160.162:9876')
-# producer.set_session_credentials('XXX', 'XXXX', 'ALIYUN') # No need to call this function if you don't use Aliyun.
+producer.set_namesrv_addr('122.51.44.31:9876')
 producer.start()
 
 msg = Message('YOUR-TOPIC')
--- backend/rocketmq/pullconsumer.py
+++ backend/rocketmq/pullconsumer.py
@@ -4,8 +4,7 @@ from rocketmq.client import PullConsumer
 consumer = PullConsumer('CID_XXX')
 consumer.set_namesrv_domain('http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet')
 # For ip and port name server address, use `set_namesrv_addr` method, for example:
-# consumer.set_namesrv_addr('127.0.0.1:9887')
-consumer.set_session_credentials('XXX', 'XXXX', 'ALIYUN') # No need to call this function if you don't use Aliyun.
+consumer.set_namesrv_addr('122.51.44.31:9887')
 consumer.start()
 
 for msg in consumer.pull('YOUR-TOPIC'):
